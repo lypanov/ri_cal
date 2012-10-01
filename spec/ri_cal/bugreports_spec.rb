9fc84d1ecfeb55b4ab8d90c0576b804ede12483f
myobie <nathan@myobie.com>
2012-10-01 16:01:22 -0700

    Fix a bug that was introduced when version was moved

--- spec/ri_cal/bugreports_spec.rb
+++ spec/ri_cal/bugreports_spec.rb
@@ -271,9 +271,9 @@ END:VCALENDAR)
     calendar = RiCal.Calendar
     calendar.add_x_property 'x_wr_calname', 'Lifetracker'
     calendar.export.should == %Q(BEGIN:VCALENDAR
+VERSION:2.0
 PRODID;X-RICAL-TZSOURCE=TZINFO:-//com.denhaven2/NONSGML ri_cal gem//EN
 CALSCALE:GREGORIAN
-VERSION:2.0
 X-WR-CALNAME:Lifetracker
 END:VCALENDAR
 )
